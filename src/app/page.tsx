5551aaea688ab27598cc79e27ebb50dc8401aff9
AxolotlJ-Dev <kalelcg.eg@gmail.com>
2024-05-26 09:12:11 -0600

    posible solucion de localstorage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,18 +6,20 @@ import Navbar from "@/components/NavBar";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [items, setItems] = useState(() => {
-    const savedItems = localStorage.getItem("items");
-    if (savedItems) {
-      return JSON.parse(savedItems);
-    }
-    return [];
-  });
+  const [items, setItems] = useState([]);
   
   useEffect(() => {
+    // Este código se ejecutará solo en el lado del cliente
+    const savedItems = localStorage.getItem("items");
+    if (savedItems) {
+      setItems(JSON.parse(savedItems));
+    }
+  }, []);
+
+  useEffect(() => {
+    // Actualiza localStorage cada vez que cambien los items
     localStorage.setItem("items", JSON.stringify(items));
   }, [items]);
-  
 
   return (
     <main className="">
